ef16ba7d819795b96727dbe9af579279cd89d1d9
nirgluzman <nir.gluzman@gmail.com>
2024-05-18 21:24:52 +0200

    outputting meal categories

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,25 @@
+import { FlatList } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES} // an array (or array-like list) of items to render.
+      keyExtractor={item => item.id} // used to extract a unique key for a given item at the specified index.
+      renderItem={renderCategoryItem} // function called for each item in the data array and returns a React component (JSX) to be rendered.
+    />
+  );
+}
+
+export default CategoriesScreen;
