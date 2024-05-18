fb63b7a1c334c3c5bf0633f1254137bdc1000571
nirgluzman <nir.gluzman@gmail.com>
2024-05-18 22:26:47 +0200

    add basic setup for React Navigation

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 
+// Routing and navigation for Expo and React Native apps, https://reactnavigation.org/docs/getting-started#wrapping-your-app-in-navigationcontainer
+import { NavigationContainer } from '@react-navigation/native';
+
+// Native Stack Navigator, https://reactnavigation.org/docs/native-stack-navigator
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <CategoriesScreen />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='MealsCategories'
+            component={CategoriesScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
