2de0a3e140ce1f38743f85926ba0a285a05efd3c
nirgluzman <nir.gluzman@gmail.com>
2024-05-19 21:36:57 +0200

    add style prop to allow styling override

--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-function MealDetails({ duration, complexity, affordability }) {
+function MealDetails({ duration, complexity, affordability, style }) {
   return (
-    <View style={styles.details}>
+    <View style={[styles.details, style]}>
       <Text style={styles.detailItem}>{duration}m</Text>
       <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
       <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
