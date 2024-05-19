145cbef2e338d2b869ddadd288fa17f29a693986
nirgluzman <nir.gluzman@gmail.com>
2024-05-19 22:37:28 +0200

    add style to MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,10 @@
-import { Image, Text, View } from 'react-native';
-
-import { MEALS } from '../data/dummy-data';
+import { StyleSheet, Image, Text, View, ScrollView } from 'react-native';
 
 import MealDetails from '../components/MealDetails.js';
+import Subtitle from '../components/MealDetail/Subtitle.js';
+import List from '../components/MealDetail/List.js';
+
+import { MEALS } from '../data/dummy-data';
 
 function MealDetailScreen({ route }) {
   // get the mealId from the route params; see route prop reference, https://reactnavigation.org/docs/route-prop
@@ -12,24 +14,57 @@ function MealDetailScreen({ route }) {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return (
-    <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
-      {selectedMeal.ingredients.map(ingredient => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text>Steps</Text>
-      {selectedMeal.steps.map(step => (
-        <Text key={step}>{step}</Text>
-      ))}
-    </View>
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32
+  },
+  
+  // we must set the image size in order to display images fetched from url.
+  image: {
+    width: '100%',
+    height: 350
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white'
+  },
+
+  detailText: {
+    color: 'white'
+  },
+
+  listOuterContainer: {
+    alignItems: 'center'
+  },
+
+  listContainer: {
+    width: '80%'
+  }
+});
