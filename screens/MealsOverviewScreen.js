a4271876db257d5f41f3464c91daaff8ec53fbd7
nirgluzman <nir.gluzman@gmail.com>
2024-05-20 15:14:17 +0200

    add a comment on categoryTitle inside useLayoutEffect block

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -23,6 +23,7 @@ function MealsOverviewScreen({
   });
 
   // useLayoutEffect is a hook which is called BEFORE the component is rendered, i.e. the effect is happening simultaneously when the componet is rendered.
+  // we can move categoryTitle outside useLayoutEffect block and wrap it in the useMemo hook; see https://www.udemy.com/course/react-native-the-practical-guide/learn/lecture/31197708?%24deeplink_path=udemy%3A%2F%2Fdiscover%3Fredirect%3Ddiscussions%26courseId%3D1436092%26discussionId%3D21822106&~channel=email&~stage=published&utm_campaign=email&utm_medium=email&utm_source=sendgrid.com&_branch_match_id=1321085568133348184&_branch_referrer=H4sIAAAAAAAAA02P62rDMAyFn8b5l6R1SnaBMAZbYX8Ge4Kg2losajvGl4a9%2FeR2bAODrE%2FH1jkm55Ae%2B75odF8dhNBZ8uf%2BHbePginT6p%2BEPGjEUPkcIJvpqhXDs5BHPpqSWi8YxXCMqCmiymJ4qbSkxO%2BTkKNaS0z4pnmwPwzj7kEy%2FJNcB3J%2FL%2BV%2BNzbi7lUZ8B7thA7IVpAyLDiFcrKUDOqGPX2CtSdQ57lEO5ka49fStm3dLZBaHfe39XyJCCq3HjJdsM0G2xAZkALbLoV0lViE6Lk2JbtZgQtAi%2F8xUpHjjMX9A4k%2FVzgl9HqJpOvKb3%2FMC9NVAQAA#questions/21822106/
   useLayoutEffect(() => {
     const categoryTitle = CATEGORIES.find(category => category.id === catId).title;
 
