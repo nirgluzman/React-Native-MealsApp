86da2ac07bcbe2858c80831ddfa7a6de14d3b4de
nirgluzman <nir.gluzman@gmail.com>
2024-05-20 18:05:28 +0200

    manage info on FavoritesScreen with Context

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,39 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import MealsList from '../components/MealsList/MealsList.js';
+import { FavoritesContext } from '../store/context/favorites-context.js';
+
+import { MEALS } from '../data/dummy-data.js';
 
 function FavoritesScreen() {
-  return <Text>FavoritesScreen</Text>;
+  // import context provider for favorites
+  const favMealsCtx = useContext(FavoritesContext);
+
+  // get favorites meals
+  const favoriteMeals = MEALS.filter(meal => favMealsCtx.ids.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    // no favorites
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet</Text>
+      </View>
+    );
+  } else return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
